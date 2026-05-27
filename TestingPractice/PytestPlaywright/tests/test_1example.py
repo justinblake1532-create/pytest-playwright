5d29c6cd6e9da465441f36cb39396d68f46f785e
Justin Blake <justin.blake1532@gmail.com>
2026-05-27 10:05:24 -0400

    testing

--- TestingPractice/PytestPlaywright/tests/test_1example.py
+++ TestingPractice/PytestPlaywright/tests/test_1example.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect
 def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title("Playwright")
+    expect(page).to_have_title("Playwright");
 
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
